a931755b7b78077e9f0f8cc7bcb4b94f42934c2e
Matej Bevec <mb9706@nsc-login.ijs.si>
2021-01-16 12:07:14 +0100

    Code ready for MPI, added OpenMP parallelization to updateParallel

--- parallel.c
+++ parallel.c
@@ -5,18 +5,21 @@
 
 #include "FreeImage.h"
 #include "omp.h"
-#include "mpi.h"
+//#include "mpi.h"
 
-#define H 64
-#define W 64
-#define ITER 64 //number of iterations
+#define H 32
+#define W 32
+#define ITER 20//number of iterations
 #define P_ST 0.01 //prob. of fire starting in a cell at init
 #define P_BD 0.6 //prob. of burning cell burning down
 #define P_H 0.58 //constant spreading prob.
 #define IMAGE_PATH "./temp/" //image sequence is stored here
 
-int grid[H][W];
-int next_grid[H][W];
+#define X_EDGE 1 //width of the horizontal "edge" of neighbouring cells to exhange
+#define Y_EDGE 0 //width of the vertical edge (has to be 0 or same as X_edge)
+
+//int grid[H][W];
+//int next_grid[H][W];
 int t;
 
 char symbols[5] = {' ',' ','.','&','*'};
@@ -40,26 +43,25 @@ double rnd(){
 }
 
 // Initialize cells (to 1 or 2)
-void init(char* grid){
+void init(char** grid, double density){
     for(int y = 0; y < H; y++)
         for(int x = 0; x < W; x++){
-            grid[y*H+x] = rand() % 2;
+            grid[y][x] = rnd() < density ? 2 : 1;
         }
 }
 
 // Start the fire in some cells: n - number of sparks
-void spark(char* grid, int n){
-    int numSparks = 0;
+void spark(char** grid, int n){
     int y0, x0;
-    while (numSparks < n){
+    for(int i = 0; i < n; i++){
         y0 = rand() % H;
-        x0 = rand() % W;
-        grid[y0*H+W] = 3;
+        x0 = rand() % W ;
+        grid[y0][x0] = 3;
     }
 }
 
 // GET STATE OF A CELL AT t+1
-int next_state(int y, int x, int t){
+int next_state(char** grid, int y, int x, int t){
 
     if (grid[y][x] == 1)
         return 1;
@@ -85,27 +87,39 @@ int next_state(int y, int x, int t){
 }
 
 // Update states for whole grid (one iteration)
-void update(){
-    for(int y = 0; y < H; y++)
-        for(int x = 0; x < W; x++)
-            next_grid[y][x] = next_state(x,y,t);
+// void update(){
+//     for(int y = 0; y < H; y++)
+//         for(int x = 0; x < W; x++)
+//             next_grid[y][x] = next_state(x,y,t);
 
-    for(int y = 0; y < H; y++)
-        for(int x = 0; x < W; x++)
-            grid[y][x] = next_grid[y][x];
-}
+//     for(int y = 0; y < H; y++)
+//         for(int x = 0; x < W; x++)
+//             grid[y][x] = next_grid[y][x];
+// }
 
 // Parallel state update for whole grid
-void updateParallel(){
-    #pragma omp parallel for schedule(guided) private(sum, sum2) num_threads(NTHREADS)
-    for(int y = 0; y < H; y++)
-        for(int x = 0; x < W; x++)
-            next_grid[y][x] = next_state(x,y,t);
-    
+void updateParallel(char** grid, char** next_grid, int li){
+    int e = X_EDGE - 1 - li;
+
+    // najhitreje je z -c16
+    #pragma omp parallel
+    {
+
+        #pragma omp for collapse(2) schedule(static)
+        for(int y = -e; y < H+e; y++)
+            for(int x = -e; x < W+e; x++)
+                next_grid[y][x] = next_state(grid,x,y,t);
+
+        #pragma omp for collapse(2) schedule(static)
+        for(int y = -e; y < H+e; y++)
+            for(int x = -e; x < W+e; x++)
+                grid[y][x] = next_grid[y][x];
+    }
 }
 
+
 // Print the grid in current state
-void print_grid(){
+void print_grid(char** grid){
     for(int y = 0; y < H; y++){
         for(int x = 0; x < W; x++){
             printf("%c", symbols[grid[y][x]]);
@@ -114,23 +128,96 @@ void print_grid(){
     }
 }
 
+// Returns grid that can be accessed like [i][j], and has "out-of-bound" edges allocated
+char** alloc_grid(int h, int w, int x_edge, int y_edge){
+    char* alloc = (char *) malloc((h+2*x_edge) * (w+2*y_edge) * sizeof(char));
+    char** grid = (char **) malloc((h+2*x_edge) * sizeof(char *));
+
+    for(int i = 0; i < (h+2*x_edge); i++)
+            grid[i] = &alloc[i*(w+2*y_edge)] + y_edge;
+    grid += x_edge;
+
+    return grid;
+}
+
+// Exhange edge data with parallel processes
+void exchange_data(char** grid){
+
+}
+
+void send_xedge(){
+    //grid edge to buffer and send
+}
+
+void send_yedge(){
+    //grid edge to buffer and send
+}
+
+void recv_xedge(){
+    //receive buffer, buffer to grid edge
+}
+
+void recv_yedge(){
+    //receive buffer, buffer to grid edge
+}
+
+void send_corner(){
+
+}
+
+void recv_corner(){
+
+}
+
 
 int main(int argc, char* argv[]){
 
-    int myid, numOfProc
-    MPI_Status status;
+    int P, myid;
+    //MPI_Status status;
 
-    char* globalGrid = (char *)malloc(H * W * sizeof(unsigned char));
+    // ! has to be allocated in process #1 and saved to a shared file or smh
+    //char* globalGrid = (char *)malloc(H * W * sizeof(unsigned char));
+
+    // Allocate the "band" for this process - sneaky version - upam a OMP parallel dela s tem
+    // char* local_grid = (char *) malloc(H * W * sizeof(char))
+
+
+    char** grid = alloc_grid(H, W, X_EDGE, Y_EDGE);
+    char** next_grid = alloc_grid(H, W, X_EDGE, Y_EDGE);
 
     // Initialize grid and spark fire in some cells
-    init(globalGrid);
-    spark(globalGrid, 20);
+    init(grid, 0.7);
+    spark(grid, 3);
+
 
     // ================== MPI ==================
 
-    // Configure MPI paralellization based on input arguments
-    MPI_Init(&argc, &argv);    
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numOfProc);
+    // Configure MPI parallelization based on input arguments
+    // MPI_Init(&argc, &argv);    
+    // MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    // MPI_Comm_size(MPI_COMM_WORLD, &P);
 
+    
+    // 1. Exchange edge data
+    // 2. Run local iterations
+    // 3. Repeat
+
+    double t_start = omp_get_wtime();
+
+    for(t = 0; t < ITER; t++){
+        if (t % X_EDGE == 0){
+
+            // EXHANGE DATA WITH MPI
+        }
+
+        print_grid(grid); 
+        printf("\n");
+        //render_grid(); 
+
+        int li = t % X_EDGE; //local iterations since the last edge exchange
+        updateParallel(grid, next_grid, li);
+    }
+
+    double t_end = omp_get_wtime();
+    printf("Elapsed time = %f\n", t_end-t_start);
 }
